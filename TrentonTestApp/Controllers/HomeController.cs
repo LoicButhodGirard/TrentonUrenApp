bc3e08693c0d1aba699316ff035404127913884e
alex <alexander.benerink@outlook.com>
2019-05-24 13:58:06 +0200

    git template updated for branch

--- TrentonTestApp/Controllers/HomeController.cs
+++ TrentonTestApp/Controllers/HomeController.cs
@@ -20,10 +20,8 @@ namespace TrentonTestApp.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Urenregistratie()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
